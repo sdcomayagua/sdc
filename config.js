485590051e3ead6d361e6bacd4448b8c88b9c23d
sdcomayagua <sdcomayagua@gmail.com>
2026-01-21 15:27:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,9 @@ window.SDC_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxx07ynQmhXH0FB7JeJFPfxYhcXxszggv3oK8TEPy5wv2_zM6kYYIcdi2YEj_DrFzKImw/exec",
   DEFAULT_WHATSAPP: "+50431517755",
   CURRENCY: "Lps",
-
-  // Tarifas nacionales
   NATIONAL_PREPAGO: 110,
   NATIONAL_CONTRA_ENTREGA: 160,
-
-  // PINs locales para abrir admin.html (acepta cualquiera)
   ADMIN_PINS: ["199311", "202528"],
-
-  // Local permitido (entrega local)
   LOCAL_ALLOW: [
     "Comayagua|Comayagua",
     "Comayagua|Ajuterique",
