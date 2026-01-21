898be8323c62e3adfe84e501a135aeac8a7f55cd
sdcomayagua <sdcomayagua@gmail.com>
2026-01-20 23:35:38 -0600

    Delete assets directory

--- assets/js/api.js
+++
@@ -1,8 +0,0 @@
-// assets/js/api.js (simple y estable)
-const API_URL = "https://script.google.com/macros/s/AKfycbytPfD9mq__VO7I2lnpBsqdCIT119ZT0zVyz0eeVjrJVgN_q8FYGgmqY6G66C2m67Pa4g/exec";
-
-async function apiGetAll() {
-  const res = await fetch(API_URL, { cache: "no-store" });
-  if (!res.ok) throw new Error(`API HTTP ${res.status}`);
-  return await res.json();
-}
