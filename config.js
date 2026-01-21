7c823718788c675ce7943c37db841319aa41b307
sdcomayagua <sdcomayagua@gmail.com>
2026-01-21 14:57:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
-// ===== CONFIG GLOBAL =====
 window.SDC_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxx07ynQmhXH0FB7JeJFPfxYhcXxszggv3oK8TEPy5wv2_zM6kYYIcdi2YEj_DrFzKImw/exec",
   DEFAULT_WHATSAPP: "+50431517755",
   CURRENCY: "Lps",
   NATIONAL_PREPAGO: 110,
   NATIONAL_CONTRA_ENTREGA: 160,
-
-  // PIN LOCAL (solo oculta admin). Cámbialo a tu gusto:
   ADMIN_PIN: "SDC2026",
-
   LOCAL_ALLOW: [
     "Comayagua|Comayagua",
     "Comayagua|Ajuterique",
