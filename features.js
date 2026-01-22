4712084cf4ed3b29b649cc35f927740bef2dc6e4
sdcomayagua <sdcomayagua@gmail.com>
2026-01-22 12:26:33 -0600

    Actualizar features.js

--- features.js
+++ features.js
@@ -1,11 +1,10 @@
-// features.js
 window.SDC_FEATURES = {
-  productTabs: true,     // Tabs en modal (Descripción/Specs/Videos)
-  buyNow: true,          // Botón Comprar ahora
-  swipeClose: true,      // Swipe down para cerrar modales (móvil)
-  modalFx: true,         // Animaciones premium
-  pcListColumns: true,   // Lista en PC con columnas
-  voiceSearch: true,     // Buscar por voz
-  favorites: true,       // Favoritos
-  swipeImages: true      // Swipe horizontal de imágenes (móvil)
+  productTabs: false,
+  buyNow: false,
+  swipeClose: false,
+  modalFx: false,
+  pcListColumns: false,
+  voiceSearch: false,
+  favorites: false,
+  swipeImages: false
 };
